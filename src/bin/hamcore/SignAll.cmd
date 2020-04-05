7ebda08cb915edac57b19a3e3ee253264221d318
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2020-04-05 13:48:53 +0900

    hamcore にファイルを追加

---
+++ src/bin/hamcore/SignAll.cmd
@@ -0,0 +1,14 @@
+C:\sec\vpn4\current\VPN4\Main\bin\BuildUtil.exe /Cmd:SignCode hookldr.exe /COMMENT:"PacketiX Desktop VPN"
+
+C:\sec\vpn4\current\VPN4\Main\bin\BuildUtil.exe /Cmd:SignCode screenhooks32.dll /COMMENT:"PacketiX Desktop VPN"
+
+C:\sec\vpn4\current\VPN4\Main\bin\BuildUtil.exe /Cmd:SignCode urdpclient2.exe /COMMENT:"PacketiX Desktop VPN"
+
+C:\sec\vpn4\current\VPN4\Main\bin\BuildUtil.exe /Cmd:SignCode urdpserver2.exe /COMMENT:"PacketiX Desktop VPN"
+
+
+C:\sec\vpn4\current\VPN4\Main\bin\BuildUtil.exe /Cmd:SignCode urdpclient.exe /COMMENT:"PacketiX Desktop VPN"
+
+C:\sec\vpn4\current\VPN4\Main\bin\BuildUtil.exe /Cmd:SignCode urdpserver.exe /COMMENT:"PacketiX Desktop VPN"
+
+C:\sec\vpn4\current\VPN4\Main\bin\BuildUtil.exe /Cmd:SignCode wm_hooks.dll /COMMENT:"PacketiX Desktop VPN"
