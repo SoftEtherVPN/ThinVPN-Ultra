e53df10e5fbb2b6c60672a7a1ec3e0a77c6963ba
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2020-06-14 17:56:21 +0900

    BUILD_NUMBER 9798 VERSION 16 BUILD_NAME beta6preview3 BUILD_DATE 20200614_175355

--- src/Cedar/Cedar.h
+++ src/Cedar/Cedar.h
@@ -133,7 +133,7 @@
 #define	CEDAR_VER					16
 
 // Build Number
-#define	CEDAR_BUILD					9796
+#define	CEDAR_BUILD					9798
 
 // Beta number
 //#define	BETA_NUMBER					3
@@ -154,10 +154,10 @@
 // Specifies the build date
 #define	BUILD_DATE_Y		2020
 #define	BUILD_DATE_M		6
-#define	BUILD_DATE_D		1
-#define	BUILD_DATE_HO		0
-#define	BUILD_DATE_MI		30
-#define	BUILD_DATE_SE		12
+#define	BUILD_DATE_D		14
+#define	BUILD_DATE_HO		17
+#define	BUILD_DATE_MI		53
+#define	BUILD_DATE_SE		55
 
 // Tolerable time difference
 #define	ALLOW_TIMESTAMP_DIFF		(UINT64)(3 * 24 * 60 * 60 * 1000)
